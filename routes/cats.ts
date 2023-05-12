41165b0033119ac6d4c28e535346fcba2a84b586
takobrown <94686168+takobrown@users.noreply.github.com>
2023-05-12 14:04:58 +0000

    Create record

--- routes/cats.ts
+++ routes/cats.ts
@@ -1,10 +1,11 @@
 import Router, { RouterContext } from "koa-router";
+import bodyParser from "koa-bodyparser";
 
 const cats = [
-  {title: 'Cat1', fullText: 'detail1'},
-  {title: 'cat2', fullText: 'detail2'},
-  {title: 'cat3', fullText: 'detail3'},
-  {title: 'cat4', fullText: 'detail4'},
+  { title: 'Cat1', fullText: 'detail1' },
+  { title: 'cat2', fullText: 'detail2' },
+  { title: 'cat3', fullText: 'detail3' },
+  { title: 'cat4', fullText: 'detail4' },
 ]
 
 const router = new Router({ prefix: '/api/v1/cats' });
@@ -16,6 +17,11 @@ const getAll = async (ctx: RouterContext, next: any) => {
 }
 
 const createRecord = async (ctx: RouterContext, next: any) => {
+  let { title, fullText } = ctx.request.body;
+  let newRecord = { title: title, fullText: fullText };
+  cats.push(newRecord);
+  ctx.status = 201;
+  ctx.body = newRecord;
   await next();
 }
 
@@ -33,7 +39,7 @@ const deleteRecord = async (ctx: RouterContext, next: any) => {
 
 //Endpoint
 router.get('/', getAll);
-router.post('/', createRecord);
+router.post('/', bodyParser(), createRecord);
 router.get('/:id', getById);
 router.put('/:id', updateRecord);
 router.delete('/:id', deleteRecord);
