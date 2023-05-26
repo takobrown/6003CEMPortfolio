45d2e5424a7cefcf6cb5860564c1ad955d9f8792
takobrown <94686168+takobrown@users.noreply.github.com>
2023-05-26 18:29:36 +0000

    User function

--- models/users.ts
+++ models/users.ts
@@ -5,3 +5,50 @@ export const findByUsername = async (username: string) => {
   const user = await db.run_query(query, [username]);
   return user;
 }
+
+export const getAllUsers = async () => {
+  const query = 'SELECT * FROM users';
+  const users = await db.run_query(query, null);
+  return users;
+}
+
+export const addUser = async (user: any) => {
+  const keys = Object.keys(user);
+  const values = Object.values(user);
+  const columns = keys.join(',');
+  const placeholders = values.map(() => '?').join(',');
+
+  const query = `INSERT INTO users (${columns}) VALUES (${placeholders})`;
+  try {
+    await db.run_query(query, values);
+    return { status: 201 };
+  } catch (err: any) {
+    return err;
+  }
+}
+
+export const updateUser = async (id: number, user: any) => {
+  const keys = Object.keys(user);
+  const values = Object.values(user);
+  const setStatement = keys.map((key) => `${key} = ?`).join(', ');
+
+  const query = `UPDATE users SET ${setStatement} WHERE id = ?`;
+  values.push(id);
+
+  try {
+    await db.run_query(query, values);
+    return { status: 200 };
+  } catch (err: any) {
+    return err;
+  }
+}
+
+export const deleteUser = async (id: number) => {
+  const query = 'DELETE FROM users WHERE id = ?';
+  try {
+    await db.run_query(query, [id]);
+    return { status: 200 };
+  } catch (err: any) {
+    return err;
+  }
+}
--- routes/users.ts
+++ routes/users.ts
@@ -1,3 +1,7 @@
+/// Add User
+// Update User Info
+// Delete User
+
 import Router, { RouterContext } from "koa-router";
 import bodyParser from "koa-bodyparser";
 import * as model from "../models/users";
@@ -5,14 +9,8 @@ import * as model from "../models/users";
 const router = new Router({ prefix: '/api/v1/users' });
 
 
-
 const findByUsername = async (ctx: RouterContext, next: any) => {
   let username = ctx.params.username;
-  /*if((id < cats.length +1) && (id > 0)){
-    ctx.body = cats [id-1];
-  } else {
-    ctx.status = 404;
-  }*/
   let users = await model.findByUsername(username);
   if (users.length) {
     ctx.body = users[0];
@@ -22,8 +20,56 @@ const findByUsername = async (ctx: RouterContext, next: any) => {
   await next();
 }
 
+//Get All user
+const getAllUsers = async (ctx: RouterContext, next: any) => {
+  const users = await model.getAllUsers();
+  ctx.body = users;
+  await next();
+}
+
+const addUser = async (ctx: RouterContext, next: any) => {
+  const user = ctx.request.body;
+  const result = await model.addUser(user);
+  if (result.status === 201) {
+    ctx.status = 201;
+  } else {
+    ctx.status = 500;
+  }
+  await next();
+}
+
+const updateUser = async (ctx: RouterContext, next: any) => {
+  const id = parseInt(ctx.params.id);
+  const user = ctx.request.body;
+  const result = await model.updateUser(id, user);
+  if (result.status === 200) {
+    ctx.status = 200;
+  } else {
+    ctx.status = 500;
+  }
+  await next();
+}
+
+const deleteUser = async (ctx: RouterContext, next: any) => {
+  const id = parseInt(ctx.params.id);
+  const result = await model.deleteUser(id);
+  if (result.status === 200) {
+    ctx.status = 200;
+  } else {
+    ctx.status = 500;
+  }
+  await next();
+}
+
+
 
 //router.get('/', getAll);
 router.get('/:username', findByUsername);
+router.use(bodyParser());
+router.get('/', getAllUsers);
+router.post('/', addUser);
+router.put('/username', updateUser);
+router.delete('/username', deleteUser);
+
 
 export { router };
